927cd79c30bc53220dca5c1a61a01dedf794ca92
RAYDAN GASPAR <raydangaspar@gmail.com>
2021-03-20 12:37:44 -0300

    seir notebook

--- model_seir/params.py
+++ model_seir/params.py
@@ -1,24 +1,18 @@
 from lmfit import Parameters
 
 
+# tuple (<value>, <vary>)
 def create_params(S0, E0, I0, R0, N, beta, gamma, delta):
     # determina quem vai variar
     params = Parameters()
 
-    params.add("S0", value=S0, vary=False)
-    params.add("E0", value=E0, vary=False)
-    params.add("I0", value=I0, vary=False)
-    params.add("R0", value=R0, vary=False)
-    params.add("N", value=N, vary=False)
-    params.add("beta", value=beta, vary=True)
-    params.add("gamma", value=gamma, vary=True)
-    params.add("delta", value=delta, vary=False)
-    # add pessoas que foram para uti
-    # add mortes sem ir pra uti
-    # add mortes pós uti
-
-    # em outros arquivos...
-    # Somar 2 erros quadrados: Infectados gerais (BH) +(ou média) Infectados para UTI
-    # Fazer minimize das duas curvas
+    params.add("S0", value=S0[0], vary=S0[1])
+    params.add("E0", value=E0[0], vary=E0[1])
+    params.add("I0", value=I0[0], vary=I0[1])
+    params.add("R0", value=R0[0], vary=R0[1])
+    params.add("N", value=N[0], vary=N[1])
+    params.add("beta", value=beta[0], vary=beta[1])
+    params.add("gamma", value=gamma[0], vary=gamma[1])
+    params.add("delta", value=delta[0], vary=delta[1])
 
     return params
